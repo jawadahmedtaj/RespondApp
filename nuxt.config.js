a1edb4d61a45c2c3d85e00060aad78436d9c0105
Jawad Ahmed <jawadahmedtaj@gmail.com>
2023-10-14 19:16:05 +0500

    lib update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,24 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
-    '@invictus.codes/nuxt-vuetify', 'nuxt-icon', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@vee-validate/nuxt'
+    'vuetify-nuxt-module', 'nuxt-icon', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@vee-validate/nuxt'
   ],
   devtools: { enabled: true },
   vuetify: {
-    /* vuetify options */
-    vuetifyOptions: {
-      // @TODO: list all vuetify options
-    },
-
     moduleOptions: {
-      /* nuxt-vuetify module options */
-      treeshaking: true | false,
-      useIconCDN: true | false,
-
-      /* vite-plugin-vuetify options */
-      styles: true | 'none' | 'expose' | 'sass',
-      autoImport: true | false,
-      useVuetifyLabs: true | false,
+      /* module specific options */
+    },
+    vuetifyOptions: {
+      /* vuetify options */
+    },
+    vuetifyOptions: {
+      directives: true
     }
-  }
+  },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,25 @@
 <template>
-    <div>
-        Index
-    </div>
+    <v-container fluid>
+        <v-row justify="space-around">
+            <v-col class="d-flex align-center">
+                <draggable v-model="myArray" group="people" @start="drag = true" @end="drag = false" item-key="id">
+                    <template #item="{ element }">
+                        <div class="text-h1">{{ element.name }}</div>
+                    </template>
+                </draggable>
+            </v-col>
+            <v-col class="d-flex align-center">2</v-col>
+            <v-col class="d-flex align-center">3</v-col>
+        </v-row>
+    </v-container>
 </template>
+
+<script setup>
+import draggable from 'vuedraggable'
+
+const drag = ref(false)
+const myArray = ref([{ name: 'a', id: 0 }, { name: 'b', id: 1 }, { name: 'c', id: 2 }, { name: 'd', id: 3 }, { name: 'e', id: 4 }, { name: 'f', id: 5 }, { name: 'g', id: 6 }])
+
+</script>
+
+<style lang="scss" scoped></style>
